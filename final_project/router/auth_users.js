7921c3322aefca075937299fae8ef0a3ee9d0033
sherelynn <sherelynnb@missionreadyhq.com>
2024-11-07 12:30:04 +1300

    Rename file and variables

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,30 +1,32 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-let books = require("./booksdb.js");
-const regd_users = express.Router();
+const express = require("express")
+const jwt = require("jsonwebtoken")
+let books = require("./books_db.js")
+const reg_users = express.Router()
 
-let users = [];
+let users = []
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = username => {
+  // returns boolean
+  // write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => {
+  // returns boolean
+  // write code to check if username and password match the one we have in records.
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+reg_users.post("/login", (req, res) => {
+  // write your code here
+  return res.status(300).json({ message: "Yet to be implemented" })
+})
 
 // Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+reg_users.put("/auth/review/:isbn", (req, res) => {
+  // write your code here
+  return res.status(300).json({ message: "Yet to be implemented" })
+})
 
-module.exports.authenticated = regd_users;
-module.exports.isValid = isValid;
-module.exports.users = users;
+module.exports.authenticated = reg_users
+module.exports.isValid = isValid
+module.exports.users = users
