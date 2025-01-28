3c551b4e3908bc657555390b8d802b2d680ade42
sherelynn <sherelynnb@missionreadyhq.com>
2025-01-29 11:53:50 +1300

    Add middleware to authenticate users

--- final_project/index.js
+++ final_project/index.js
@@ -17,11 +17,26 @@ app.use(
   })
 )
 
+// Middleware to authenticate users
 app.use("/customer/auth/*", function auth(req, res, next) {
-  // Write the authentication mechanism here
+  // Get the authorization object stored in the session
+  if (req.session.authorization) {
+    token = req.session.authorization["accessToken"] // Retrieve the token from the authorization object
+    jwt.verify(token, "access", (err, user) => {
+      // Use JWT to verify token
+      if (!err) {
+        req.user = user
+        next()
+      } else {
+        return res.status(401).json({ message: "User not authenticated " })
+      }
+    })
+  } else {
+    return res.status(403).json({ message: "User not logged in" })
+  }
 })
 
-const PORT = 5000
+const PORT = 5001
 
 app.use("/customer", customer_routes)
 app.use("/", general_routes)
